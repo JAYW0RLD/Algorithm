9c9ad261bd7e611c1fe21393bd132af48bd7f63a
김재영 <125888877+JAYW0RLD@users.noreply.github.com>
2025-07-07 19:49:23 +0900

    [Silver III] Title: 전쟁 - 땅따먹기, Time: 6084 ms, Memory: 4592 KB -BaekjoonHub

--- 백준/Silver/1270. 전쟁 － 땅따먹기/전쟁 － 땅따먹기.cc
+++ 백준/Silver/1270. 전쟁 － 땅따먹기/전쟁 － 땅따먹기.cc
@@ -1,35 +1,37 @@
 #include <iostream>
-#include <string>
-#include <map>
+#include <unordered_map>
+#include <cstdio>  // scanf, printf
 using namespace std;
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    
-    int n; cin >> n;
-    for(int idx=0;idx<n;idx++){
-        int Ti; cin >> Ti;
+    int n;
+    scanf("%d", &n);
 
-        map<long long,int> m;
-        int max=-1;
-        long long max_i=-1;
-        for(int i=0;i<Ti;i++){
+    for (int idx = 0; idx < n; idx++) {
+        int Ti;
+        scanf("%d", &Ti);
+
+        unordered_map<long long, int> m;
+        int max_cnt = -1;
+        long long max_id = -1;
+
+        for (int i = 0; i < Ti; i++) {
             long long sol;
-            cin >> sol;
+            scanf("%lld", &sol);  // long long은 %lld로 받아야 함
             m[sol] += 1;
-            if(max < m[sol]){
-                max = m[sol];
-                max_i = sol;
+
+            if (max_cnt < m[sol]) {
+                max_cnt = m[sol];
+                max_id = sol;
             }
         }
 
-        if(max > Ti/2){
-            cout << max_i << "\n";
-        }
-        else{
-            cout << "SYJKGW\n";
+        if (max_cnt > Ti / 2) {
+            printf("%lld\n", max_id);  // 출력도 %lld
+        } else {
+            printf("SYJKGW\n");
         }
     }
+
     return 0;
 }
